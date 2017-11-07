ca17692a0bbcac78c027f5ef40d896eb8b62eb75
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2017-11-07 17:01:21 -0600

    Force name change build prefix projects with z

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet restore ./tests/Solcast.Tests.csproj
-dotnet build ./tests/Solcast.Tests.csproj
-dotnet test ./tests/Solcast.Tests.csproj
+dotnet restore ./tests/zSolcast.Tests.csproj
+dotnet build ./tests/zSolcast.Tests.csproj
+dotnet test ./tests/zSolcast.Tests.csproj
