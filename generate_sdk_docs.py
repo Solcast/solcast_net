89248a6d2f3eeb4669b396b5ed80fe7546c2c128
Nikolay Petrov <nikolay@solcast.com.au>
2024-11-21 04:06:37 +0000

    changed net6.0 to net8.0

--- generate_sdk_docs.py
+++ generate_sdk_docs.py
@@ -34,7 +34,7 @@ CSPROJ_CONTENT = f"""
 <Project Sdk="Microsoft.NET.Sdk">
   <PropertyGroup>
     <OutputType>Exe</OutputType>
-    <TargetFramework>net6.0</TargetFramework>
+    <TargetFramework>net8.0</TargetFramework>
   </PropertyGroup>
   <ItemGroup>
     <ProjectReference Include="{SOLCAST_PROJECT_PATH}" />
